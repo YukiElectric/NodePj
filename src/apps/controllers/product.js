bad4079f033698c030ad64fb0b05dc59a0ae866d
YukiElectric <phamthanhlap2003.hha@gmail.com>
2023-10-22 19:09:24 +0700

    add pagination.js and pagination.ejs

--- src/apps/controllers/product.js
+++ src/apps/controllers/product.js
@@ -1,14 +1,25 @@
 const ProductModel = require("../models/product");
 const CategoryModel = require("../models/category");
+const pagination = require("../../common/pagination");
 
 const index = async (req, res) => {
-    const product = await ProductModel.find({}).populate({path : "cat_id"});
-    const products = product.map((item) => {
-        item.price = eval(item.price).toLocaleString('vn-VN');
-        console.log(item.price);
+    const limit = 10;
+    const page = parseInt(req.query.page) || 1;
+    const skip = limit*(page -1);
+    const data = await ProductModel.find({}).sort({_id : -1}).limit(limit).skip(skip).populate({path : "cat_id"});
+    const products = data.map((item) => {
+        item.price = (Math.ceil(eval(item.price)/1000)*1000).toLocaleString('vi-VN',{style: 'currency',
+        currency: 'VND'});
         return item;
     })
-    res.render("admin/products/product", {products});
+    const totalRows = await ProductModel.find().countDocuments();
+    const totalPages = Math.ceil(totalRows/limit);
+    const pages = pagination(page, totalPages);
+    const next = page + 1;
+    const hasNext = page < totalPages ? true : false;
+    const prev = page - 1;
+    const hasPrev = page > 1 ? true : false;
+    res.render("admin/products/product", {products, pages, page, totalPages, next, hasNext, prev, hasPrev});
 }
 
 const create = (req, res) => {
